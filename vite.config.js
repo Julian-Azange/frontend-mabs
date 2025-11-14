f47bd1b3b01baaab662d413284629658c6f3a586
JulianPDP <julianazange@gmail.com>
2025-11-14 17:29:58 -0500

    MOD: cambios del login

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react()
-  ]
+  ],
+  proxy: {
+    '/api': {
+      target: 'https://server-mabs-xo9s.onrender.com',
+      changeOrigin: true,
+      secure: false,
+    }
+  }
 })
